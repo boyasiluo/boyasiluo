228f33b7d2f916af6ae246e7408a86f32639e2e1
luoye <boyasiluo@163.com>
2022-12-26 04:02:49 +0800

    beadhouse

---
+++ src/main/java/com/example/beadhouse/BeadhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.beadhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeadhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beadhouse/BeadhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beadhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
